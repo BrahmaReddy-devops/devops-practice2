15f28eadd7faee9a2e8d83021ef8ed35d72e9661
Brahma Reddy <brahma8834@gmail.com>
2023-01-03 22:58:15 +0530

    shell script file

--- 02-print.sh
+++ 02-print.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
 echo hello world 123"**** hi 123 ****"
+
+#color code syn : echo -e "\e[colmMessege\e[0m"
+#red-31 g-32 y-33 b-34 m-35 c-36
+echo -e "\e[31mMessege\e[0m"
+echo -e "\e[32mMessege\e[0m"
+echo -e "\e[33mMessege\e[0m"
+echo -e "\e[34mMessege\e[0m"
